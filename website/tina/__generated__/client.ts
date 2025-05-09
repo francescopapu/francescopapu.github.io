4ff0d2a62cf5d8c693bb663da61978db1c9ea370
francescopapu <francescom.papulino@gmail.com>
2025-05-09 22:50:59 +0200

    Update .gitignore

--- website/tina/__generated__/client.ts
+++ website/tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: 'C:/Users/franc/Documents/PapuIM/hugo_website/website/tina/__generated__/.cache/1746823701754', url: 'https://content.tinajs.io/1.5/content/b462f927-78c3-417b-a3b4-296f12309228/github/main', token: 'afdb18b75c1608da2e80228fd79db082c24e9c63', queries,  });
+export const client = createClient({ cacheDir: 'C:/Users/franc/Documents/PapuIM/hugo_website/website/tina/__generated__/.cache/1746823938112', url: 'https://content.tinajs.io/1.5/content/b462f927-78c3-417b-a3b4-296f12309228/github/main', token: 'afdb18b75c1608da2e80228fd79db082c24e9c63', queries,  });
 export default client;
   
