360fb1479525ea2a8f559f0d51e737fc8e60cddd
francescopapu <francescom.papulino@gmail.com>
2025-05-10 02:03:26 +0200

    update

--- website/tina/__generated__/client.ts
+++ website/tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: 'C:/Users/franc/Documents/PapuIM/hugo_website/website/tina/__generated__/.cache/1746829260422', url: 'https://content.tinajs.io/1.5/content/b462f927-78c3-417b-a3b4-296f12309228/github/main', token: 'afdb18b75c1608da2e80228fd79db082c24e9c63', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'undefined', queries,  });
 export default client;
   
